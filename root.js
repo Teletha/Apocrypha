db22df3a8e8a91466ecd1e8e69d3013b1feb9650
teletha <teletha@users.noreply.github.com>
2024-12-22 00:16:46 +0000

    deploy: 82ad8eebc2df6901c2e6de158408c58371c3ed1d

---
+++ root.js
@@ -0,0 +1,51 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"apocrypha"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Apocrypha",
+			"packageName": "apocrypha",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Apocrypha.ÅssignableAll",
+			"packageName": "apocrypha",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Apocrypha.ÅssignableName",
+			"packageName": "apocrypha",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Apocrypha.ÅssignableÅrbitrary",
+			"packageName": "apocrypha",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Apocrypha.Ìnstantiator",
+			"packageName": "apocrypha",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ApocryphaModel",
+			"packageName": "apocrypha",
+			"type": "Functional"
+		}
+	]
+}
